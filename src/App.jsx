04b99ea3098c326b72dfa5fb25deb151aaf56e2c
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-23 04:22:16 +0300

    Add navigation routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
+import Navigation from './components/Navigation';
+
 function App() {
   return (
-    <h1>Bookstore Project</h1>
-  )
+    <Router>
+      <div className="w-11/12 mx-auto h-screen">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/categories" element={<BookForm />} />
+      </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
